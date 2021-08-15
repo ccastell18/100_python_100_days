4ac99893f8471e9d1583e0edd2054615c126c774
Chris Castellano <c.castellano18@gmail.com>
2021-08-14 20:57:14 -0500

    day8 done

--- Day8_Cipher/main.py
+++ Day8_Cipher/main.py
@@ -4,13 +4,6 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-
-shift = int(input("Type the shift number:\n"))
-
-shift = shift % 26
-
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
@@ -26,8 +19,23 @@ def caesar(start_text, shift_amount, cipher_direction):
     print(f"The {cipher_direction}d word is {end_text}")
 
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
 
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    result = input(
+        "Type 'yes' if you would like to go again.  Type 'no' to exit. \n")
+    if result == "no":
+        should_continue = False
+        print("Goodbye.")
+
+# original
 
 # def encrypt(plain_text, shift_amount):
 #     cipher_text = ""
