e9101064bab8d1c2fce45d3c877a70d78048ec48
Chris Castellano <c.castellano18@gmail.com>
2022-03-19 18:12:18 -0500

    day32"

---
+++ Day32/Project2/main.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = "c.castellano18@yahoo.com"
+PASSWORD = "ymxb suas bxlb eptx"
+# the date
+today = datetime.now()
+today_tuple = (today.month, today.day)
+# dataframe dict
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[Name]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.mail.yahoo.com", 587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=birthday_person["email"],
+                            msg=f"Subject: Happy Birthday!\n\n{contents}")
